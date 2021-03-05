eccdf0dce77cb94a2b58f50b17990206943bdef1
Filip Míka <mikafil@zcu.cz>
2021-03-05 20:14:22 +0100

    [ridani async / await funkci

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const tokenizer = new nlp.WordTokenizer();
 const stemmer = nlp.PorterStemmer;
 const wordnet = new nlp.WordNet();
 var Lemmer = require('lemmer');
-const WeatherApi = "http://api.openweathermap.org/data/2.5/weather?appid=febe9884711fbda5d8830f609b2edbbb";
+const WeatherApi = "http://api.openweathermap.org/data/2.5/weather?appid=febe9884711fbda5d8830f609b2edbbb&units=metric";
 
 
 var port = process.env.PORT || config.get('PORT');
@@ -24,7 +24,7 @@ const bot = new BootBot({
 
 bot.hear(['hello', 'hi'], (payload, chat) => {
 	console.log('The user said "hello" or "hi"!');
-	chat.say('Hi ');
+	chat.say('Hi _ 002');
 });
 
 
@@ -32,19 +32,44 @@ bot.hear(['hello', 'hi'], (payload, chat) => {
 
 
 bot.hear(/weather (.*)/i, (payload, chat, data) => {
-		console.log(data);
-		const movieName = data.match[1];
-		console.log(movieName);
+	console.log(data);
+	const placeName = data.match[1];
+	
+	console.log(placeName);
 
-		const askName = (convo) => {
-			convo.ask(`What's your name?`, (payload, convo) => {
-				const text = payload.message.text;
-				convo.set('name', text);
-				console.log("name = " + text);
-				convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
-			});
-		};
+	const askName = (convo) => {
+		convo.ask(`Would you like know forecast for  tomerrow?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set("answer", text);
+			convo.set('state', 2);
+			console.log("answer is : " + text);
+			if (text === 'yes') {
+				console.log("yes for forecvast");
+				convo.say("OK, find forecast");
+			} else if (text === 'no') {
 
+            }
+			//convo.say(`Oh, your name is ${text}`).then(() => askSelfish(convo));
+		});
+	};
+
+
+	const askSelfish = (convo) => {
+		convo.ask(`Do you like me?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set("answer", text);
+			convo.set('state', 2);
+			console.log("answer is : " + text);
+			if (text === 'yes') {
+				console.log("yes for forecvast");
+				convo.say("OK, find forecast")
+			} else if (text === 'no') {
+
+			}
+			convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
+		});
+	};
+	/*
 		conversation.ask(`What's your name?`, (payload, conversation) => {
 			const text = payload.message.text;
 			conversation.set('name', text);
@@ -52,11 +77,17 @@ bot.hear(/weather (.*)/i, (payload, chat, data) => {
 			conversation.end();
 		});
 
+	*/
+	
 
 	chat.conversation((conversation => {
-		
-
-		fetch(WeatherApi + "&q=" + movieName).then(res => res.json()).then(json => {
+		conversation.set("today", Date.now());
+		conversation.set("placeName", placeName);
+		conversation.set("_chat", chat);
+		conversation.set("_payload", payload);
+		conversation.set("_data", data);
+		/*
+		await fetch(WeatherApi + "&q=" + placeName).then(res => res.json()).then(json => {
 			if (json["cod"] == 200) {
 				if (json.hasOwnProperty('main')) {
 					if (json['main'].hasOwnProperty('temp')) {
@@ -65,15 +96,64 @@ bot.hear(/weather (.*)/i, (payload, chat, data) => {
 				} else {
 					
                 }
-			} else {
+			} else {  
 				chat.say("I'm sorry I naven't read data.")
             }
 			console.log("Search result is " + JSON.stringify(json));
+			convo.set("placeName", placeName);
+			convo.set("state", 1);
 		})
+		conversation.say("He here");
 		askName(conversation);
 		console.log("conversation ended");
+		*/
+		doFirst(conversation);
 	}))
 })
+
+
+async function doFirst(conv) {
+	let chat = conv.get("_chat");
+	let rsponse = 
+		await fetch(WeatherApi + "&q=" + conv.get("placeName")).then(res => res.json()).then(json => {
+			if (json["cod"] == 200) {
+				if (json.hasOwnProperty('main')) {
+					if (json['main'].hasOwnProperty('temp')) {
+						chat.say("The tempeatrure is " + json['main']['temp']);
+					}
+				} else {
+					
+				}
+			} else {  
+				chat.say("I'm sorry I naven't read data.")
+			}
+			console.log("Search result is " + JSON.stringify(json));
+			conv.set("state", 1);
+		});
+	let forecast = await doAskForForecast(conv);
+	
+}
+
+async function doAskForForecast(conv){
+	
+	let chat = conv.get("_chat");
+	let payload = conv.get("_payload");
+	await conv.ask(`Would you like know forecast for  tomerrow?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set("answer", text);
+			convo.set('state', 2);
+			console.log("answer is : " + text);
+			if (text === 'yes') {
+				console.log("yes for forecvast");
+				convo.say("OK, find forecast");
+			} else if (text === 'no') {
+
+            }
+			//convo.say(`Oh, your name is ${text}`).then(() => askSelfish(convo));
+		});
+}
+
+
 
 
 
